bd36c25797ed013b74d7284e46cb819255529b15
Lidia Ataupillco <ataupill@ualberta.ca>
2019-03-07 19:19:16 -0700

    more changes

--- assignment3.py
+++ assignment3.py
@@ -21,7 +21,7 @@ def display_pages(conn, c):
     print(df.iloc[:last_page,0:1])
     selection =''
 
-    # Show all papers
+    # Show all papers until indicate it to quit
     while (True):
         print("\nSelect 'n' for next page, 'p' for previous or 'q' to quit")
         selection = input(">")
@@ -29,7 +29,6 @@ def display_pages(conn, c):
         if selection.upper() == 'N':
             if (last_page) >= size:
                 print("Page does not exist. Try again.")
-
             else :
                 first_page = last_page
                 last_page += 6
@@ -38,7 +37,6 @@ def display_pages(conn, c):
         elif selection.upper() == 'P':
             if (first_page) <= 0:
                 print("Page does not exist. Try again.")
-                
             else:
                 first_page -=5
                 last_page -=6
@@ -52,22 +50,24 @@ def display_pages(conn, c):
     conn.commit()
     return
 
+# show all papers 
 def show_current_reviewers(conn, c):
     display_pages(conn, c)
-
-    paper = input("Choose the number of the paper to be selected\n")
+    print("\nChoose the number of the paper to be selected")
+    paper = input(">")
     p_title = (paper,)
     # allow one paper to be selected
     c.execute("select reviewer from papers p, reviews r where p.id=r.paper and p.title=?;",p_title)
     rows = c.fetchall()
     size_rows = len(rows)
 
-    # if 
-    if (size_rows == 0):
-        print("Reviewer not assigned")
-    else:
-       for i in range(0,size_rows):
+    try:
+        # Display the email of all reviewers that have reviewed the paper
+        for i in range(0,size_rows):
            print(rows[i][0])
+    except Exception as e:
+        #if empty
+        print("Reviewer not assigned")
     
     conn.commit()
     return
@@ -84,11 +84,13 @@ def show_potential_reviewers(conn, c):
     rows = c.fetchall()
     size_rows = len(rows)
     
-    if (size_rows == 0):
-        print("Potential reviewers not assigned")
-    else:
-       for i in range(0,size_rows):
+    try:
+        # display the email of all reviewers that have reviewed the paper
+        for i in range(0,size_rows):
            print(rows[i][0])
+    except Exception as e:
+        # if empty
+        print("Potential reviewers not assigned")
     
     reviewer = input("Choose a reviewer")
     orig, imp, sound = input("\nInput scores for originality, importance and soundness: ").split()
@@ -158,7 +160,8 @@ def show_author_participation(conn, c):
         if size == 0:
             print("There's no input")
 
-        author = input("Provide the author\n")
+        print("Provide the author\n")
+        author = input(">")
         
         found = False
         # look for author
